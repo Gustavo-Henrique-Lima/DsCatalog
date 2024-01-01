ab09e2e1b750794655c898d9b33f3c89b84a181b
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-01 18:11:29 -0300

    feat: Documenting API with swagger

--- dscatalog/src/main/java/com/gustavonascimento/dscatalog/DscatalogApplication.java
+++ dscatalog/src/main/java/com/gustavonascimento/dscatalog/DscatalogApplication.java
@@ -3,7 +3,11 @@ package com.gustavonascimento.dscatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API para loja 'DSCatalog'", version = "1.0", description = "API criada para o projeto de um catalogo de produtos criado durante o bootcamp Spring 3.0 da escola de programação DevSuperior."))
 public class DscatalogApplication {
 
 	public static void main(String[] args) {
