7c2af9a9caefab19e991b71fbccf71f52dad84b2
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-01 18:01:41 -0300

    feat: seeding data base

---
+++ dscatalog/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category(name) VALUES ('Livros');
+INSERT INTO tb_category(name) VALUES ('Eletrônica');
+INSERT INTO tb_category(name) VALUES ('Computadores');
+INSERT INTO tb_category(name) VALUES ('Celulares');
