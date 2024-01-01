095a4f3dbec7c2491c8a734588d379974bba059c
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-01 18:59:02 -0300

    feat: SFetching category by id and generated custom exception handling

--- dscatalog/src/main/java/com/gustavonascimento/dscatalog/controllers/CategoryController.java
+++ dscatalog/src/main/java/com/gustavonascimento/dscatalog/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,16 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping(value = "")
 	public ResponseEntity<List<CategoryDTO>> findAll() {
-		List<CategoryDTO> categories = service.findAll();
-		return ResponseEntity.ok(categories);
+		List<CategoryDTO> entity = service.findAll();
+		return ResponseEntity.ok(entity);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO entity = service.findById(id);
+		return ResponseEntity.ok(entity);
 	}
 }
--- dscatalog/src/main/java/com/gustavonascimento/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/gustavonascimento/dscatalog/services/CategoryService.java
@@ -1,12 +1,16 @@
 package com.gustavonascimento.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.gustavonascimento.dscatalog.entities.Category;
 import com.gustavonascimento.dscatalog.entities.dto.CategoryDTO;
 import com.gustavonascimento.dscatalog.repositories.CategoryRepository;
+import com.gustavonascimento.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,8 +20,15 @@ public class CategoryService {
 
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
-		List<Category> categories = repository.findAll();
-		List<CategoryDTO> categoriesDto = categories.stream().map(x -> new CategoryDTO(x)).toList();
-		return categoriesDto;
+		List<Category> objs = repository.findAll();
+		List<CategoryDTO> entities = objs.stream().map(x -> new CategoryDTO(x)).toList();
+		return entities;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+		return new CategoryDTO(entity);
 	}
 }
