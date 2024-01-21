d8c93d2b60ff66d59ba16a33a6c2e0c64edffebc
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-20 22:34:16 -0300

    build: Update pom.

--- dscatalog/src/main/java/com/gustavonascimento/dscatalog/config/SecurityConfig.java
+++ dscatalog/src/main/java/com/gustavonascimento/dscatalog/config/SecurityConfig.java
@@ -3,11 +3,24 @@ package com.gustavonascimento.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
-	
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
